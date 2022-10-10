0a3a694c384a51d4b06003fb300e32c97e0dc0dd
Ryuichi Watanabe <ryucrosskey@gmail.com>
2022-10-11 00:51:03 +0900

    Create main.go

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func standardizeSpaces(s string) string {
+	return strings.Join(strings.Fields(s), " ")
+}
+
+func main() {
+
+	doc, err := goquery.NewDocument("https://event.cloudnativedays.jp/cndt2022/talks?utm_source=twregular2210&utm_medium=twitter&utm_campaign=CNDT2022CFP")
+	if err != nil {
+		panic("htmlの取得に失敗しました")
+	}
+	title := doc.Find("body#page-top > div#wrapper > div.container > div.row > table.table > tbody > tr")
+	// fmt.Println(title.Html())
+	title.Each(func(i int, s *goquery.Selection) {
+		author := s.Find(fmt.Sprintf("td.td-class-%d > ul", i+1))
+		author.Each(func(i int, s *goquery.Selection) {
+			fmt.Println(standardizeSpaces(s.Text()))
+		})
+	})
+}
